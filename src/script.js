0ad195d72d45a7168bb3ad6757675ecf801064d9
Phi Ndlovu <ndlovusiphilangenkosi@gmail.com>
2025-04-16 03:45:13 +0200

    removed initial CSS & JS

--- src/script.js
+++ src/script.js
@@ -1,2 +1,1 @@
-let note = document.querySelector("#notification");
-note.innerHTML = `⚠Please note that this page is still under construction. Visit again later to see the progress⚠`;
+//Removed script, will add later
