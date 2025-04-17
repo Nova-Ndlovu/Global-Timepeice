03e8a2ae44f145311e1d9807e7df85ebadfa9dd6
Phi Ndlovu <ndlovusiphilangenkosi@gmail.com>
2025-04-17 04:03:56 +0200

    Update JS for functional time
    Added JS to the project to show the actual time for the two slots printed

--- src/script.js
+++ src/script.js
@@ -1,1 +1,29 @@
-//Removed script, will add later
+function updateTime() {
+  //For the chosen location
+
+  let chosenLocation = document.querySelector("#chosen-location");
+  let chosenDate = chosenLocation.querySelector(".date");
+  let chosenTime = chosenLocation.querySelector(".time");
+  let chosenLocationTime = moment().tz("America/Los_Angeles");
+
+  chosenDate.innerHTML = moment().format("MMMM Do YYYY");
+  chosenTime.innerHTML = `${chosenLocationTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+
+  //for the current location
+
+  let currentLocation = document.querySelector("#current-location");
+  let currentDate = currentLocation.querySelector(".date");
+  let currentTime = currentLocation.querySelector(".time");
+  //let currentLocationTime = moment.tz.guess();
+  //moment.tz("Africa/Johannesburg");
+
+  currentDate.innerHTML = moment().format("MMMM Do YYYY");
+  currentTime.innerHTML = moment
+    .tz(moment.tz.guess())
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
